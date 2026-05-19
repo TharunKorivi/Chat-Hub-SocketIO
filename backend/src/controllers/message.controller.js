61204366e38e9adf4a2f7de74082a7bec699376b
TharunKorivi <tharun.korivi04@gmail.com>
2026-05-19 13:26:50 +0530

    Added multiple sockets emit possible

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
-import { getReceiverSocketId, io } from "../lib/socket.js";
+import { emitToUser, getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -67,7 +67,7 @@ export const sendMessage = async (req, res) => {
     const receiverSocketId = getReceiverSocketId(chatToId);
 
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
+      emitToUser(receiverSocketId, "newMessage", newMessage);
     }
 
     res.status(201).json(newMessage);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,63 +4,71 @@ import jwt from "jsonwebtoken";
 
 let io;
 
-const userSocketMap = {};
+// Map<userId, Set<socketId>>
+const userSocketMap = new Map();
 
-export const getReceiverSocketId = (receiverId) =>
-  userSocketMap[receiverId];
+export const getReceiverSocketId = (receiverId) => {
+  const sockets = userSocketMap.get(receiverId);
+  if (!sockets || sockets.size === 0) return null;
+  return [...sockets][0];
+};
+
+export const emitToUser = (receiverId, event, data) => {
+  const sockets = userSocketMap.get(receiverId);
+  if (!sockets) return;
+  sockets.forEach((socketId) => {
+    io.to(socketId).emit(event, data);
+  });
+};
 
 export const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin:"http://localhost:5173",
-      credentials:true
+      origin: process.env.ORIGIN || "http://localhost:5173",
+      credentials: true,
     },
   });
 
+  io.use((socket, next) => {
+    try {
+      const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+      const token = cookies.jwt;
+      if (!token) return next(new Error("Unauthorized"));
 
-io.use((socket, next) => {
-  try {
-    const cookies = cookie.parse(
-      socket.handshake.headers.cookie || ""
-    );
-
-    const token = cookies.jwt;
-
-    if (!token) {
-      return next(new Error("Unauthorized"));
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      socket.userId = decoded.userId;
+      next();
+    } catch (error) {
+      next(new Error("Unauthorized"));
     }
-
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
-
-    socket.userId = decoded.userId;
-
-    next();
-  } catch (error) {
-    next(new Error("Unauthorized"));
-  }
-});
+  });
 
   io.on("connection", (socket) => {
-    console.log("A user is connected", socket.id);
+    console.log("A user connected", socket.id);
 
     const userId = socket.userId;
 
     if (userId) {
-      userSocketMap[userId] = socket.id;
+      if (!userSocketMap.has(userId)) {
+        userSocketMap.set(userId, new Set());
+      }
+      userSocketMap.get(userId).add(socket.id);
     }
 
-    // send online users to everyone
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", [...userSocketMap.keys()]);
 
     socket.on("disconnect", () => {
       console.log("A user disconnected", socket.id);
 
-      delete userSocketMap[userId];
+      if (userId) {
+        const sockets = userSocketMap.get(userId);
+        if (sockets) {
+          sockets.delete(socket.id);
+          if (sockets.size === 0) userSocketMap.delete(userId);
+        }
+      }
 
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+      io.emit("getOnlineUsers", [...userSocketMap.keys()]);
     });
   });
 };
